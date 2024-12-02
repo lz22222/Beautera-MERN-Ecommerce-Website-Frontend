cea961478ec9ef9c7836921d6b13238fab6b48bf
“Ling <lzhuo2@ualberta.ca>
2024-12-01 17:27:24 -0700

    ..

--- src/pages/shop/ProductCards.jsx
+++ src/pages/shop/ProductCards.jsx
@@ -4,47 +4,6 @@ import RatingStars from '../../components/RatingStars';
 import { useDispatch } from 'react-redux'
 import { addToCart } from '../../redux/features/cart/cartSlice'
 
-
-import trend1 from '../../assets/trend1.avif';
-import trend2 from '../../assets/trend2.png';
-import trend3 from '../../assets/trend3.avif';
-import trend4 from '../../assets/trend4.avif';
-import trend5 from '../../assets/trend5.avif';
-import trend6 from '../../assets/trend6.avif';
-import trend7 from '../../assets/trend7.avif';
-import trend8 from '../../assets/trend8.png';
-import trend9 from '../../assets/trend9.avif';
-import trend10 from '../../assets/trend10.png';
-import placeholder from '../../assets/header.jpeg';
-
-export const getImageSrc = (imagePath) => {
-    switch (imagePath) {
-        case '/assets/trend1.avif':
-            return trend1;
-        case '/assets/trend2.png':
-            return trend2;
-        case '/assets/trend3.avif':
-            return trend3;
-        case '/assets/trend4.avif':
-            return trend4;
-        case '/assets/trend5.avif':
-            return trend5;
-        case '/assets/trend6.avif':
-            return trend6;
-        case '/assets/trend7.avif':
-            return trend7;
-        case '/assets/trend8.png':
-            return trend8;
-        case '/assets/trend9.avif':
-            return trend9;
-        case '/assets/trend10.png':
-            return trend10;
-        default:
-            return placeholder;
-    }
-};
-
-
 const ProductCards = ({products}) => {
 	const dispatch = useDispatch()
 
@@ -72,9 +31,8 @@ const ProductCards = ({products}) => {
 				<div key={index} className='product__card'>
 					<div className='relative'>
 						<Link to={`/shop/${product._id}`}>
-
-                            <img
-                                src={getImageSrc(product.image)}
+						    <img
+                                src={product.image} 
                                 alt={product.name}
                                 className="w-full h-full object-cover hover:scale-105 transition-all duration-300 aspect-square"
                             />
