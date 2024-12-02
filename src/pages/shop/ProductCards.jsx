6fbe2495b8cac8f23a039e9c85a8e191c2d307ae
“Ling <lzhuo2@ualberta.ca>
2024-12-01 18:29:08 -0700

    update

--- src/pages/shop/ProductCards.jsx
+++ src/pages/shop/ProductCards.jsx
@@ -31,7 +31,7 @@ const ProductCards = ({products}) => {
 				<div key={index} className='product__card'>
 					<div className='relative'>
 						<Link to={`/shop/${product._id}`}>
-						<img src={product.image} alt="product image" className='max-h-96 md:h-64 w-full object-cover hover:scale-105 transition-all duration-300' />
+						<img src={product.image} alt="product image" className="w-full h-full object-cover hover:scale-105 transition-all duration-300 aspect-square" />
                         </Link>
 
                         <div className='hover:block absolute top-3 right-3'>
